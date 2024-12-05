d575f185dfb60d594cb204f7b6c4f1c2bc74347d
MathieuKPC06 <mathieu.leclair@kpconsulting.fr>
2024-12-05 17:50:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,4 @@ if ingredients_list:
         st.success(f'Congrats {name_on_order}! Your Smoothie is ordered! ✅')
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
